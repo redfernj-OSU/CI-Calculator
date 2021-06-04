066c458b8f633f60a50e48a861d82effe90ef8b3
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-06-03 23:36:08 -0700

    fixed linting errors

--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,13 @@ Tests for calc app
 
 import calculator
 
+
 class TestCalculatorApp:
     def test_add(self):
-        assert 5 == calculator.add(3,2)
+        assert 5 == calculator.add(3, 2)
 
     def test_subtract(self):
-        assert 5 == calculator.subtract(10,5)
+        assert 5 == calculator.subtract(10, 5)
 
     def test_multiply(self):
-        assert 4 == calculator.multiply(2,2)
+        assert 4 == calculator.multiply(2, 2)
